2ebf9eaa21f0313da2491c2b2a6dab917f5e9a55
dhiraj-7655 <dhiraj1.kumar@lntinfotech.com>
2024-10-04 18:34:15 +0530

    json added

--- src/app/high-chart-sunburn/high-chart-sunburn.component.ts
+++ src/app/high-chart-sunburn/high-chart-sunburn.component.ts
@@ -3,8 +3,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import Sunburst from 'highcharts/modules/sunburst';
-import vecData from "../test.json";
-
+import vecData from '../test.json';
 
 /**
  * Notes
@@ -27,26 +26,42 @@ export class HighChartSunburnComponent implements OnInit {
   ngOnInit(): void {
     const data = vecData;
 
-    // data.forEach((point: any) => {
-    //   if (point.value > 5) {
-    //     // point.borderWidth = 0;
-    //     // point.color = '#cc66ff';
-    //     // point.color = '#BFBFBF';
-    //   } else {
-    //     point.borderWidth = 1; // Keep border if     value <= 6
-    //     // point.color = '#582A72';
-    //   }
-    //   // if(point.name == ' ') {
-    //   //   point.color = '#cc66ff';
-    //   // }
-    // });
+    data.forEach((point: any) => {
+      if (point.value > 17) {
+        point.borderWidth = 0;
+        // point.color = '#cc66ff';
+        // point.color = '#BFBFBF';
+      } else {
+        // point.borderWidth = 1; // Keep border if     value <= 6
+        // point.color = '#582A72';
+      }
+      // if(point.name == ' ') {
+      //   point.color = '#cc66ff';
+      // }
+    });
     this.chartOptions = {
       chart: {
         height: '100%',
       },
-      colors: ['transparent', '#C68FE6', '#86D293', '#54C392', '#15B392'],
       title: {
         text: 'Test Data',
+      },
+      plotOptions: {
+        sunburst: {
+          point: {
+            // events: {
+            //   click: function (event) {
+            //     // if (this.node.level === 1 || this.node.level === 2) {
+            //     //   // Handle click event for level 1 and level 2
+            //     //   console.log('Clicked on level', this.node.level);
+            //     // } else {
+            //     //   // Prevent click event for other levels
+            //     //   event.preventDefault();
+            //     // }
+            //   }
+            // }
+          }
+        }
       },
       series: [
         {
@@ -59,6 +74,10 @@ export class HighChartSunburnComponent implements OnInit {
           enableMouseTracking: true,
           dataLabels: {
             format: '{point.name}',
+            style: {
+              fontSize: '12px', // Change this to your desired font size
+              textOutline: 'none', // This removes the glow effect
+            },
             filter: {
               property: 'innerArcLength',
               operator: '>',
@@ -68,49 +87,66 @@ export class HighChartSunburnComponent implements OnInit {
           levels: [
             {
               level: 1,
-              color: '#00DFA2',
               dataLabels: {
                 filter: {
                   property: 'outerArcLength',
                   operator: '>',
                   value: 64,
                 },
+                style: {
+                  fontSize: '9px', // Change this to your desired font size
+                  textOutline: 'none', // This removes the glow effect
+                },
               },
               levelSize: {
                 unit: 'percentage',
                 value: 15,
               },
+              // enableMouseTracking: true,
             },
             {
               level: 2,
               colorByPoint: true,
               levelSize: {
                 unit: 'percentage',
-                value: 15,
+                value: 17,
+              },
+              dataLabels: {
+                style: {
+                  fontSize: '8px', // Change this to your desired font size
+                  textOutline: 'none', // This removes the glow effect
+                },
               },
             },
             {
               level: 3,
-              color: '#480032',
-              colorVariation: {
-                key: 'brightness',
-                to: -0.5,
-              },
               levelSize: {
                 unit: 'percentage',
-                value: 15,
+                value: 17,
+              },
+              dataLabels: {
+                style: {
+                  fontSize: '8px', // Change this to your desired font size
+                  textOutline: 'none', // This removes the glow effect
+                  color : 'white'
+                },
               },
             },
             {
               level: 4,
               levelSize: {
                 unit: 'percentage',
-                value: 20,
+                value: 3,
               },
-              color: '#00215E',
               colorVariation: {
                 key: 'brightness',
                 to: 0.5,
+              },
+              dataLabels: {
+                style: {
+                  fontSize: '12px', // Change this to your desired font size
+                  textOutline: 'none', // This removes the glow effect
+                },
               },
             },
           ],
@@ -119,6 +155,7 @@ export class HighChartSunburnComponent implements OnInit {
       ],
       tooltip: {
         headerFormat: '',
+        stickOnContact: false,
         pointFormat: '<b style="color: red">{point.value}</b>',
       },
     };
